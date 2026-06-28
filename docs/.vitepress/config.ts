b9374faac5eee0dce15e9001af346a40571591c8
crimson-med <28290546+crimson-med@users.noreply.github.com>
2026-06-29 00:49:49 +0800

    docs: dynamic copyright year + starside.io initiative credit in footer

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -91,7 +91,7 @@ export default defineConfig({
     },
     footer: {
       message: 'Released under the Apache-2.0 License.',
-      copyright: 'Copyright © 2025 Neurowire',
+      copyright: `Copyright © ${new Date().getFullYear()} Neurowire<br><a href="https://starside.io/" target="_blank" rel="noopener" class="starside-credit"><img src="https://starside.io/mars.png" alt="" width="14" height="14" /> a starside.io initiative</a>`,
     },
   },
 })
